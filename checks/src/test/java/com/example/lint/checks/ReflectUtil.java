21edbc826cc741d3646dc851ad6da9ef7213399f
cylee <cylee0909@qq.com>
2019-09-22 18:19:02 +0800

    修改target sdk

---
+++ checks/src/test/java/com/example/lint/checks/ReflectUtil.java
@@ -0,0 +1,50 @@
+package com.example.lint.checks;
+
+import java.lang.reflect.Field;
+
+public class ReflectUtil {
+
+    public static Object getFieldValue(final Object obj, final String fieldName) {
+        Field field = getAccessibleField(obj, fieldName);
+
+        if (field == null) {
+            throw new IllegalArgumentException("Could not find field [" + fieldName + "] on target [" + obj + "]");
+        }
+
+        Object result = null;
+        try {
+            result = field.get(obj);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void setFieldValue(final Object obj, final String fieldName, final Object value) {
+        Field field = getAccessibleField(obj, fieldName);
+
+        if (field == null) {
+            throw new IllegalArgumentException("Could not find field [" + fieldName + "] on target [" + obj + "]");
+        }
+
+        try {
+            field.set(obj, value);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Field getAccessibleField(final Object obj, final String fieldName) {
+        for (Class<?> superClass = obj.getClass(); superClass != Object.class; superClass = superClass.getSuperclass()) {
+            try {
+                Field field = superClass.getDeclaredField(fieldName);
+                field.setAccessible(true);
+                return field;
+            } catch (NoSuchFieldException e) {//NOSONAR
+                continue;// new add
+            }
+        }
+        return null;
+    }
+
+}
--- checks/src/test/java/com/example/lint/checks/TestDetectorTest.kt
+++ checks/src/test/java/com/example/lint/checks/TestDetectorTest.kt
@@ -4,7 +4,6 @@ import com.android.sdklib.AndroidVersion
 import com.android.tools.lint.checks.BatteryDetector
 import com.android.tools.lint.checks.infrastructure.LintDetectorTest
 import com.android.tools.lint.checks.infrastructure.TestFiles
-import com.android.tools.lint.client.api.LintClient
 import com.android.tools.lint.detector.api.Detector
 import com.android.tools.lint.detector.api.Issue
 import com.android.tools.lint.detector.api.Project
@@ -14,17 +13,31 @@ class TestDetectorTest : LintDetectorTest() {
     fun testBasic() {
         val testFile = javaClass.getResource("/BatteryTest.java").file
         val content = File(testFile).readText()
+
+        val gradleContent = File(javaClass.getResource("/build.gradle").file).readText()
         TestFiles.gradle("")
         val result = lint().
                 sdkHome(File("/Users/cylee/Library/Android/sdk")).
-                files(java(content))
-                .modifyGradleMocks { androidProject, variant ->
-//                    androidProject.defaultConfig.productFlavor.minSdkVersion = 19
-                    println(androidProject)
-                }
+                files(java(content), gradle(gradleContent))
+//                .client(object : com.android.tools.lint.checks.infrastructure.TestLintClient(){
+//                    override fun createProject(dir: File, referenceDir: File): Project {
+//                        var p = super.createProject(dir, referenceDir)
+//                        ReflectUtil.setFieldValue(null, "manifestTargetSdk", AndroidVersion(24, null))
+//                        return p;
+//                    }
+//                })
+//                .modifyGradleMocks { androidProject, variant ->
+//                    println(androidProject)
+//                }
                 .run()
 
-        result.expectWarningCount(2)
+        result.expectWarningCount(3)
+    }
+
+    override fun setUp() {
+        super.setUp()
+        ReflectUtil.setFieldValue(AndroidVersion.DEFAULT, "mApiLevel", 24);
+        ReflectUtil.setFieldValue(AndroidVersion.DEFAULT, "mCodename", "24");
     }
 
     override fun getDetector(): Detector {
